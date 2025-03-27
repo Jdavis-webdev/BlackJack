4ca69cb281ea0851533c3fc7f765d9eda354db84
Jdavis-webdev <fishrifle1234@gmail.com>
2025-03-27 17:57:32 -0500

    update12

--- blackjack-game/vite.config.ts
+++ blackjack-game/vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
 export default defineConfig({
-  plugins: [react()],
-});
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'http://my-backend.example.com',
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
+})
