d8811893ab6acecd4a50fefb48748bbd18b53cac
Jdavis-webdev <fishrifle1234@gmail.com>
2025-03-27 17:07:59 -0500

    update 9

--- blackjack-game/postcss.config.cjs
+++ blackjack-game/postcss.config.cjs
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: {
+    "postcss-preset-env": {
+      stage: 3,
+      features: {
+        appearance: false, // Disable or adjust handling of appearance
+      },
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
